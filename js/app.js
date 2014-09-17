4a8d3dbfef4e7130cc4e3f6248d056bfe865bec1
RockinRonE <ronny.ewanek@gmail.com>
2014-09-17 14:59:35 -0500

    Done!

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var i = 1;
+
+var fizzBuzz = function () {
+    for (i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            $('body').append("<p>Fizz Buzz</p>");
+            console.log("Fizz Buzz");
+        } else if (i % 3 === 0) {
+            $('body').append("<p>Fizz</p>");
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            $('body').append("<p>Buzz</p>");
+            console.log("Buzz");
+        } else {
+            $('body').append('<p>' + i + '</p>');
+            console.log(i);
+        }
+    }
+};
+
+fizzBuzz();
